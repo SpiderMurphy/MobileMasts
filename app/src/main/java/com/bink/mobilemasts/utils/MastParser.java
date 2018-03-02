6971a372ccbdcc616ac132544eb72a90add0f71a
SpiderMurphy <vincenzocaleca@gmail.com>
2018-03-02 11:15:40 +0100

    Mast Parser from csv added

---
+++ app/src/main/java/com/bink/mobilemasts/utils/MastParser.java
@@ -0,0 +1,24 @@
+package com.bink.mobilemasts.utils;
+
+import com.bink.mobilemasts.models.Mast;
+
+import java.io.Reader;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by Cyan on 02/03/2018.
+ */
+
+public class MastParser {
+    /**
+     *
+     *
+     * @param reader
+     * @param comparator
+     * @return
+     */
+    public static List<Mast> readFromCsv(Reader reader, Comparator<Mast> comparator){
+        return null;
+    }
+}

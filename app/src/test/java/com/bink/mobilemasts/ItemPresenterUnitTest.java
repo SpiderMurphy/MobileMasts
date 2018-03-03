8a7aabe53be70064a9c8e41dd3c1c03877f03ab9
SpiderMurphy <vincenzocaleca@gmail.com>
2018-03-03 11:10:38 +0100

    ItemPresenter UnitTest updated

--- app/src/test/java/com/bink/mobilemasts/ItemPresenterUnitTest.java
+++ app/src/test/java/com/bink/mobilemasts/ItemPresenterUnitTest.java
@@ -15,8 +15,48 @@ public class ItemPresenterUnitTest {
     @Test
     public void test_get_count () {
         ItemPresenter itemPresenter = new ItemPresenter();
+        assertEquals(0, itemPresenter.getCount());
+    }
 
-        itemPresenter.add(new ListItem("", ""));
+    @Test
+    public void test_add_item () {
+        ItemPresenter itemPresenter = new ItemPresenter();
+        itemPresenter.add(new ListItem<>("t1", "t1", null));
+
         assertEquals(1, itemPresenter.getCount());
+    }
+
+    @Test
+    public void test_sort_list_descending () {
+        ItemPresenter itemPresenter = new ItemPresenter();
+
+        itemPresenter.add(new ListItem<>("t1", "t1", 1));
+        itemPresenter.add(new ListItem<>("t2", "t2", 2));
+
+        itemPresenter.sort(((item, t1) -> {
+            Integer val1 = (Integer) item.getItem();
+            Integer val2 = (Integer) t1.getItem();
+
+            return val1 > val2 ? -1 : (val1 < val2 ? 1 : 0);
+        }));
+
+        assertEquals("t2", itemPresenter.get(0).getTitle());
+    }
+
+    @Test
+    public void test_sort_list_ascending (){
+        ItemPresenter itemPresenter = new ItemPresenter();
+
+        itemPresenter.add(new ListItem<>("t1", "t1", 2));
+        itemPresenter.add(new ListItem<>("t2", "t2", 1));
+
+        itemPresenter.sort(((item, t1) -> {
+            Integer val1 = (Integer) item.getItem();
+            Integer val2 = (Integer) t1.getItem();
+
+            return val1 > val2 ? 1 : (val1 < val2 ? -1 : 0);
+        }));
+
+        assertEquals("t2", itemPresenter.get(0).getTitle());
     }
 }
